8e42711b993c57dbf0b6035fd2a991ea76dfcc6e
peeushb <peeushb@gmail.com>
2016-08-26 11:04:47 +0100

    Changed Tomcat version (to 7.0.70)

--- windows-tomcat/SetupTomcat.ps1
+++ windows-tomcat/SetupTomcat.ps1
@@ -1,3 +1,3 @@
 New-Item "c:\temp" -ItemType Directory
-Invoke-WebRequest "https://spvlabtemplates.blob.core.windows.net/software/apache-tomcat-8.5.4.exe" -OutFile "c:\temp\tomcat8.exe"
-Start-Process "C:\temp\tomcat8.exe" -ArgumentList "/S" -Wait
+Invoke-WebRequest "https://spvlabtemplates.blob.core.windows.net/software/apache-tomcat-7.0.70.exe" -OutFile "c:\temp\tomcat7.exe"
+Start-Process "C:\temp\tomcat7.exe" -ArgumentList "/S" -Wait
