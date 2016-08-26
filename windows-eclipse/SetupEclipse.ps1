c1ea232fca7553cff8c4af13e0db2f41f1dbf6e5
peeushb <peeushb@gmail.com>
2016-08-26 13:54:04 +0100

    Issue fix

--- windows-eclipse/SetupEclipse.ps1
+++ windows-eclipse/SetupEclipse.ps1
@@ -1,5 +1,6 @@
 New-Item "c:\temp" -ItemType Directory
 Invoke-WebRequest "https://spvlabtemplates.blob.core.windows.net/software/eclipse-jee-neon-R-win32-x86_64.zip" -OutFile "C:\temp\eclipse.zip"
+New-Item "C:\eclipse" -ItemType Directory
 Expand-Archive -Path "C:\temp\eclipse.zip" -DestinationPath "C:\eclipse"
 $oldPath=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINESystemCurrentControlSetControlSession ManagerEnvironment" -Name PATH).Path
 $newPath=$oldPath+";C:\eclipse"
