2ef5bdb9bb6e872a7e50ec77ed55a65c81c4a13c
peeushb <peeushb@gmail.com>
2016-08-27 11:22:25 +0100

    Updated Eclipse version

--- windows-eclipse/SetupEclipse.ps1
+++ windows-eclipse/SetupEclipse.ps1
@@ -1,25 +1,27 @@
 # Create temp directory
 New-Item "C:\temp" -ItemType Directory -ErrorAction SilentlyContinue
-Invoke-WebRequest "https://spvlabtemplates.blob.core.windows.net/software/eclipse-jee-neon-R-win32-x86_64.zip" -OutFile "C:\temp\eclipse.zip"
+Invoke-WebRequest "https://spvlabtemplates.blob.core.windows.net/software/eclipse-jee-luna-SR2-win32-x86_64.zip" -OutFile "C:\temp\eclipse-luna.zip"
 
 # Extract Eclipse files to new folder
 $shell = New-Object -com shell.application
-$zip = $shell.NameSpace("C:\temp\eclipse.zip")
-foreach($item in $zip.items())
+$zip = $shell.NameSpace("C:\temp\eclipse-luna.zip")
+# Ensure folder is empty before extracting files
+Remove-Item "c:\eclipse\*" -Recurse -ErrorAction SilentlyContinue
+Foreach($item In $zip.items())
 {
-    $shell.Namespace("C:\").copyhere($item)
+    $shell.Namespace("C:\").Copyhere($item)
 }
 
 # Update PATH Environment variable
-$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
-$oldPath=(Get-ItemProperty -Path "$Reg" -Name PATH).Path
-$newPath=$oldPath+";C:\eclipse"
-Set-ItemProperty -Path "$Reg" -Name PATH -Value $newPath
+#$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+#$oldPath=(Get-ItemProperty -Path "$Reg" -Name PATH).Path
+#$newPath=$oldPath+";C:\eclipse"
+#Set-ItemProperty -Path "$Reg" -Name PATH -Value $newPath
 
 # Create a Shortcut with Windows PowerShell
-$TargetFile = "C:\eclipse\eclipse.exe"
-$ShortcutFile = "$env:Public\Desktop\Eclipse.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
+#$TargetFile = "C:\eclipse\eclipse.exe"
+#$ShortcutFile = "$env:Public\Desktop\Eclipse.lnk"
+#$WScriptShell = New-Object -ComObject WScript.Shell
+#$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+#$Shortcut.TargetPath = $TargetFile
+#$Shortcut.Save()
