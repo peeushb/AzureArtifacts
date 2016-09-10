4df6574435b05223acb47a34de0ebc814991a0fd
peeushb <peeushb@gmail.com>
2016-09-10 02:44:46 +0100

    Create shortcut to ssms

--- windows-mssqlsrv2012/SetupMSSQLSrv.ps1
+++ windows-mssqlsrv2012/SetupMSSQLSrv.ps1
@@ -12,4 +12,12 @@ $ArgumentList = "/q /ACTION=Install QUIET=True /FEATURES=SQLENGINE /INSTANCENAME
     $SQLSVCPASSWORD + " /SQLSYSADMINACCOUNTS=" + $SQLSYSADMINACCOUNTS + " /AGTSVCACCOUNT=""NT AUTHORITY\Network Service"" /IACCEPTSQLSERVERLICENSETERMS /AGTSVCSTARTUPTYPE=Automatic /SQLSVCSTARTUPTYPE=Automatic"
 
 # Install MS SQL Server
-Start-Process "C:\SQLServer_11.0_Full\setup.exe" -ArgumentList $ArgumentList -Wait
+#Start-Process "C:\SQLServer_11.0_Full\setup.exe" -ArgumentList $ArgumentList -Wait
+
+# Create a Shortcut for SQL Server Management Studio with Windows PowerShell
+$TargetFile = "C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Ssms.exe"
+$ShortcutFile = "$env:Public\Desktop\SQL Server Management Studio.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
