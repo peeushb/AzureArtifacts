72a19dda96c3c568dad7f7719348d6d93ad80f18
peeushb <peeushb@gmail.com>
2016-09-10 01:34:28 +0100

    Include error action

--- windows-thinrdp/SetupThinRdp.ps1
+++ windows-thinrdp/SetupThinRdp.ps1
@@ -1,3 +1,6 @@
-New-Item "c:\temp" -ItemType Directory
+# Create temp directory
+New-Item "c:\temp" -ItemType Directory -ErrorAction SilentlyContinue
+
+# Download and install ThinRDP
 Invoke-WebRequest "https://thinrdpx.blob.core.windows.net/extension/Thinfinity_Remote_Desktop_Workstation_Setup_x64.exe" -OutFile "c:\temp\thinrdp.exe"
 Start-Process "C:\temp\thinrdp.exe" -ArgumentList "/S /v""/qn""" -Wait
