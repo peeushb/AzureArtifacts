e3a01ce7a3fb9d188df323c352e6cf6fd18d6a5a
peeushb <peeushb@gmail.com>
2016-10-07 12:01:16 +0100

    testing

--- windows-mssqlsrv2012-full/startInstall.ps1
+++ windows-mssqlsrv2012-full/startInstall.ps1
@@ -1,38 +1,39 @@
-﻿Function Get-TempPassword()
-{
-    Param(
-        [int] $length=10,
-        [string[]] $sourcedata
-    )
-
-    For ($loop=1; $loop –le $length; $loop++) 
-    {
-        $tempPassword+=($sourcedata | GET-RANDOM)
-    }
-
-    return $tempPassword
-}
-
-$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
-
-$userName = "artifactInstaller"
-$password = Get-TempPassword –length 43 –sourcedata $ascii
+﻿#Function Get-TempPassword()
+#{
+#    Param(
+#        [int] $length=10,
+#        [string[]] $sourcedata
+#    )
+#
+#    For ($loop=1; $loop –le $length; $loop++) 
+#    {
+#        $tempPassword+=($sourcedata | GET-RANDOM)
+#    }
+#
+#    return $tempPassword
+#}
+#
+#$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+#
+#$userName = "artifactInstaller"
+#$password = Get-TempPassword –length 43 –sourcedata $ascii
+$password = "Welcome123!"
 
 $cn = [ADSI]"WinNT://$env:ComputerName"
 
-# Create user
-$user = $cn.Create("User", $userName)
-$user.SetPassword($password)
-$user.SetInfo()
-$user.description = "DevTestLab artifact installer"
-$user.SetInfo()
+## Create user
+#$user = $cn.Create("User", $userName)
+#$user.SetPassword($password)
+#$user.SetInfo()
+#$user.description = "DevTestLab artifact installer"
+#$user.SetInfo()
 
 # Add user to the Administrators group
-$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
-$group.add("WinNT://$env:ComputerName/$userName")
+#$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
+#$group.add("WinNT://$env:ComputerName/$userName")
 
 $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\LabAdmin", $secPassword)
 
 $command = $PSScriptRoot + "\SetupMSSQLSrv.ps1"
 
@@ -44,8 +45,8 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME
 
-# Delete the artifactInstaller user
-$cn.Delete("User", $userName)
-
-# Delete the artifactInstaller user profile
-gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+## Delete the artifactInstaller user
+#$cn.Delete("User", $userName)
+#
+## Delete the artifactInstaller user profile
+#gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
