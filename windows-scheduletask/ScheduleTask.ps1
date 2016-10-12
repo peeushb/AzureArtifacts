2744af57b68af4f7fe82b04eb351d0b625296e87
peeushb <peeushb@gmail.com>
2016-10-12 22:55:02 +0100

    Initial version

---
+++ windows-scheduletask/ScheduleTask.ps1
@@ -0,0 +1,9 @@
+# Create temp directory
+New-Item "C:\temp" -ItemType Directory -ErrorAction SilentlyContinue
+(New-Object System.Net.WebClient).DownloadFile("https://spvlabtemplates.blob.core.windows.net/software/Auto-shutdownon-on-disconnect.xml", "C:\temp\Auto-shutdownon-on-disconnect.xml")
+
+# Import task
+$xmlfile = "C:\temp\Auto-shutdownon-on-disconnect.xml"
+$VMName = hostname
+$UserName = $VMName + "\LabAdmin"
+schtasks /Create /XML $xmlfile /RU $UserName /RP "Welcome123!" /TN "Auto shutdown on disconnect"
